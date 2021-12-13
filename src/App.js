37de352de00497345ab8a5077894a6e988fc159b
adelonmedeiros <adelonmedeiros@gmail.com>
2021-12-13 17:48:41 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+
+  const [msg, setMsg] = useState("Teste do useState");
+
+  function handleClick() {
+    setMsg("useState funcionou!");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <h1>{msg}</h1>
+      <button className="button" onClick={()=>handleClick()}>Clique Aqui</button>
+      </div>
+  )
 }
 
 export default App;
